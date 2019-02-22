aafec50f35e34810738bfa1deb2b5f45de64326d
saudahabib <saudababs00@gmail.com>
2019-02-22 15:41:32 +0300

    test js functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,36 @@
-alert(5);
+// UI logic begins here//
+var question1=  $("input:radio[name:ques1].val()");
+var question2=  $("input:radio[name:ques2].val()");
+var question3=  $("input:radio[name:ques3].val()");
+var question4=  $("input:radio[name:ques4].val()");
+var question5=  $("input:radio[name:ques5].val()");
+var marks=0
+var rights=0
+//awarding marks here//
+if(question1==="EMCAscript")  {
+  marks+=20
+  rights++
+}
+if (question2===".toObject")  {
+  marks+=20
+  rights++
+
+if(question3==="be")  {
+  marks+=20
+  rights++
+}
+if(question4==="B") {
+  marks+=20
+  rights++
+}
+if(question5==="C") {
+  marks+=20
+  rights++
+}
+var results= rights;
+alert("You have "+rights+" "+ "right questions. Your score is "+ marks);
+
+
 $(document).ready(function()  {
   $(".test").submit(function(event) {
     event.preventDefault();

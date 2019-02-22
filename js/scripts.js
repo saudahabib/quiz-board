423c602a43865265e669124f1d90fdf90b80dc02
saudahabib <saudababs00@gmail.com>
2019-02-22 23:34:32 +0300

    debug js functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,42 @@
 // UI logic begins here//
-var question1=  $("input:radio[name:ques1].val()");
-var question2=  $("input:radio[name:ques2].val()");
-var question3=  $("input:radio[name:ques3].val()");
-var question4=  $("input:radio[name:ques4].val()");
-var question5=  $("input:radio[name:ques5].val()");
-var marks=0
-var rights=0
-//awarding marks here//
-if(question1==="EMCAscript")  {
-  marks+=20
-  rights++
-}
-if (question2===".toObject")  {
-  marks+=20
-  rights++
-
-if(question3==="be")  {
-  marks+=20
-  rights++
-}
-if(question4==="B") {
-  marks+=20
-  rights++
-}
-if(question5==="C") {
-  marks+=20
-  rights++
-}
-var results= rights;
-alert("You have "+rights+" "+ "right questions. Your score is "+ marks);
-
-
 $(document).ready(function()  {
   $(".test").submit(function(event) {
     event.preventDefault();
-    alert("yass queen!");
+    var question1=  $("input:radio[name=ques1]:checked").val();
+    var question2=  $("input:radio[name=ques2]:checked").val();
+    var question3=  $("input:radio[name=ques3]:checked").val();
+    var question4=  $("input:radio[name=ques4]:checked").val();
+    var question5=  $("input:radio[name=ques5]:checked").val();
+    var marks=0;
+    var rights=0;
+    //business logic begins here//
+
+    if(question1=="1")  {
+       marks+=20;
+       rights++;
+
+     }
+     if (question2=="3")  {
+       marks+=20;
+       rights++;
+     }
+     if(question3=="4")  {
+       marks+=20;
+       rights++;
+     }
+     if(question4=="2") {
+       marks+=20;
+       rights++;
+     }
+     if(question5=="3") {
+       marks+=20;
+       rights++;
+     }
+    alert( "you have scored" +" "+marks+ "% with a total of" +" "+rights+" "+ "rights")
+
   });
+
+
 });
+
+//awarding marks here//
